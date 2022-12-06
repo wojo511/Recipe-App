cdb4a4c4de94b7d2c3151b56e05742f938f2dd00
Wojciech Koryś <wojtek.korys@wp.pl>
2022-12-06 16:14:21 +0100

    Adding JPA enumerations

---
+++ src/main/java/wojtekkorys/springframework/domain/Difficulty.java
@@ -0,0 +1,6 @@
+package wojtekkorys.springframework.domain;
+
+public enum Difficulty {
+
+    EASY, MODERATE, KIND_OF_HARD, HARD
+}
--- src/main/java/wojtekkorys/springframework/domain/Recipe.java
+++ src/main/java/wojtekkorys/springframework/domain/Recipe.java
@@ -16,14 +16,12 @@ public class Recipe {
     private String url;
     private String directions;
 
-
-//todo add
-    //private Difficulty difficulty;
-
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
     private Set<Ingredient> ingredients;
     @Lob
     private Byte[] image;
+    @Enumerated(value = EnumType.STRING)
+    private Difficulty difficulty;
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
 
@@ -113,6 +111,14 @@ public class Recipe {
 
     public void setIngredients(Set<Ingredient> ingredients) {
         this.ingredients = ingredients;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
     }
 
 }
