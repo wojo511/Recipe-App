135db7a856975eae8a9c01d0f3f66476c5deef27
Wojciech Koryś <wojtek.korys@wp.pl>
2022-12-06 16:43:29 +0100

    Adding Many to Many JPA relationships

--- src/main/java/wojtekkorys/springframework/domain/Recipe.java
+++ src/main/java/wojtekkorys/springframework/domain/Recipe.java
@@ -25,6 +25,12 @@ public class Recipe {
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
 
+    @ManyToMany
+    @JoinTable(name = "recipe_category",
+            joinColumns = @JoinColumn(name = "recipe_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id"))
+    private Set<Category> categories;
+
     public Long getId() {
         return id;
     }
@@ -121,4 +127,11 @@ public class Recipe {
         this.difficulty = difficulty;
     }
 
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Set<Category> categories) {
+        this.categories = categories;
+    }
 }
