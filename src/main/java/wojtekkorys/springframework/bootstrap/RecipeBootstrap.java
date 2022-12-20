a1461a742474da759842c3354b2aca6d51354ac8
Wojciech Koryś <wojtek.korys@wp.pl>
2022-12-20 22:56:45 +0100

    Adding Project Lombok

--- src/main/java/wojtekkorys/springframework/bootstrap/RecipeBootstrap.java
+++ src/main/java/wojtekkorys/springframework/bootstrap/RecipeBootstrap.java
@@ -1,6 +1,7 @@
 package wojtekkorys.springframework.bootstrap;
 
 
+import lombok.extern.slf4j.Slf4j;
 import wojtekkorys.springframework.domain.*;
 import wojtekkorys.springframework.repositories.CategoryRepository;
 import wojtekkorys.springframework.repositories.RecipeRepository;
@@ -9,14 +10,13 @@ import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
 
+import javax.transaction.Transactional;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-/**
- * Created by jt on 6/13/17.
- */
+@Slf4j
 @Component
 public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEvent> {
 
@@ -31,8 +31,11 @@ public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEven
     }
 
     @Override
+    @Transactional
     public void onApplicationEvent(ContextRefreshedEvent event) {
         recipeRepository.saveAll(getRecipes());
+        log.debug("Loading bootstrap data");
+
     }
 
     private List<Recipe> getRecipes() {
--- src/main/java/wojtekkorys/springframework/domain/Notes.java
+++ src/main/java/wojtekkorys/springframework/domain/Notes.java
@@ -1,45 +1,25 @@
 package wojtekkorys.springframework.domain;
 
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
 import javax.persistence.*;
 
+@Data
+@EqualsAndHashCode(exclude = {"recipe"})
 
 @Entity
 public class Notes {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     @OneToOne
     private Recipe recipe;
     @Lob
     private String recipeNotes;
 
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public Recipe getRecipe() {
-        return recipe;
-    }
-
-    public void setRecipe(Recipe recipe) {
-        this.recipe = recipe;
-    }
-
-    public String getRecipeNotes() {
-        return recipeNotes;
-    }
-
-    public void setRecipeNotes(String recipeNotes) {
-        this.recipeNotes = recipeNotes;
-    }
 }
 
 
