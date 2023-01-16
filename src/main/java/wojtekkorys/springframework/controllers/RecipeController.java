e1bc192d9c341200c11c6547c267401cbfc772d7
Wojciech Koryś <wojtek.korys@wp.pl>
2023-01-16 21:46:38 +0100

    Create a Recipe

--- src/main/java/wojtekkorys/springframework/controllers/RecipeController.java
+++ src/main/java/wojtekkorys/springframework/controllers/RecipeController.java
@@ -2,8 +2,11 @@ package wojtekkorys.springframework.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import wojtekkorys.springframework.commands.RecipeCommand;
 import wojtekkorys.springframework.services.RecipeService;
 
 import static java.lang.Long.parseLong;
@@ -20,9 +23,21 @@ public class RecipeController {
     @RequestMapping("/recipe/show/{id}")
     public String showById(@PathVariable String id, Model model){
 
-        model.addAttribute("recipe", recipeService.findById(parseLong(id)));
-
+        model.addAttribute("recipe", recipeService.findById(Long.valueOf(id)));
         return "recipe/show";
+    }
 
+    @RequestMapping("recipe/new")
+    public String newRecipe(Model model){
+        model.addAttribute("recipe", new RecipeCommand());
+
+        return "recipe/recipeform";
+    }
+
+    @PostMapping("recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand command){
+        RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
+
+        return "redirect:/recipe/show/" + savedCommand.getId();
     }
 }
